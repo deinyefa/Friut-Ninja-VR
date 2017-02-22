9a5f8a65541acbd799616a937e27dccabd67fce9
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-02-22 16:21:05 -0500

    added slicing plugin

---
+++ Assets/Scripts/SwordCutter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class SwordCutter : MonoBehaviour {
+
+	public Material capMaterial;
+
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject victim = other.collider.gameObject;
+
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+            pieces[1].AddComponent<Rigidbody>();
+
+        Destroy(pieces[1], 1);
+    }
+}
