8f4cd90d56a9710ed1899d2b15b678b1c7c352a1
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-02-28 09:07:44 -0500

    feat: included a new type of taget

---
+++ Assets/HitTarget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTarget : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "target")
+        {
+            Debug.Log("hit target");
+        }
+    }
+}
