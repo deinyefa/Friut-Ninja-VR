7fb83c388ff75d05cbabbf41c365dec163a050bf
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-02-28 09:30:28 -0500

    updated target
    plays sound when arrow hits it

--- Assets/HitTarget.cs
+++ Assets/HitTarget.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HitTarget : MonoBehaviour {
+public class HitTarget : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    public AudioClip hitTargetSound;
+
+    void Start ()
+    {
+        audioSource = GetComponent <AudioSource>();
+    }
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "target")
+        if (other.gameObject.tag == "projectile")
         {
             Debug.Log("hit target");
+            audioSource.PlayOneShot(hitTargetSound);
         }
     }
 }
